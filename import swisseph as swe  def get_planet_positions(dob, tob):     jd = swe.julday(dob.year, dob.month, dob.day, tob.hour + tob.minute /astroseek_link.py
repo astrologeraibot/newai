a15307590446f5e7b5b6a8c4d7ad3e4b18e624d3
astrologeraibot <travelking1990@gmail.com>
2025-05-26 14:38:49 +0530

    Delete import swisseph as swe  def get_planet_positions(dob, tob):     jd = swe.julday(dob.year, dob.month, dob.day, tob.hour + tob.minute  directory

--- import swisseph as swe  def get_planet_positions(dob, tob):     jd = swe.julday(dob.year, dob.month, dob.day, tob.hour + tob.minute /astroseek_link.py
+++
@@ -1,2 +0,0 @@
-def generate_astroseek_link(dob):
-    return f"https://horoscopes.astro-seek.com/birth-chart-horoscope-online/?narozeni_den={dob.day}&narozeni_mesic={dob.month}&narozeni_rok={dob.year}"
