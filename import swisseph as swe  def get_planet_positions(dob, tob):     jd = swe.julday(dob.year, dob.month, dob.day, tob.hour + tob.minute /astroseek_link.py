daa916fd587b9e3cfcb1c443e99b90e3cf03008d
astrologeraibot <travelking1990@gmail.com>
2025-05-26 14:38:15 +0530

    Create astroseek_link.py

---
+++ import swisseph as swe  def get_planet_positions(dob, tob):     jd = swe.julday(dob.year, dob.month, dob.day, tob.hour + tob.minute /astroseek_link.py
@@ -0,0 +1,2 @@
+def generate_astroseek_link(dob):
+    return f"https://horoscopes.astro-seek.com/birth-chart-horoscope-online/?narozeni_den={dob.day}&narozeni_mesic={dob.month}&narozeni_rok={dob.year}"
